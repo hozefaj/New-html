4a469b6afc4b927753020842b549c5e13d930890
hozefa <hozefarules@gmail.com>
2016-02-23 19:14:08 -0800

    fixing linting issues & bumping up package.json version

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
-
-
-
 define(function (require, exports, module) {
-    "use strict";
-    var commandName         = "newHtml";
-    var Menus               = brackets.getModule("command/Menus"),
-        CommandManager      = brackets.getModule("command/CommandManager"),
-        EditorManager       = brackets.getModule("editor/EditorManager");
-    
-    var template
+  "use strict";
+  var commandName = "newHtml",
+    Menus = brackets.getModule("command/Menus"),
+    CommandManager = brackets.getModule("command/CommandManager"),
+    EditorManager = brackets.getModule("editor/EditorManager"),
+    template;
+  
     /*
      * Implementation
      */
-    function loadhtml() {
-        template = require('text!html-template.html');
-      EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
-    }
-    /*
-     * Command
-     */
-CommandManager.register("New html", commandName, loadhtml); 
-    /*
-     * Custom menu
-     */
-var menu = Menus.addMenu("Html", "edgedocks.custom.menu" );  
- menu.addMenuItem(commandName)
+  function loadhtml() {
+    template = require('text!html-template.html');
+    EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
+  }
+  /*
+   * Command
+   */
+  CommandManager.register("New html", commandName, loadhtml);
+  /*
+   * Custom menu
+   */
+  var menu = Menus.addMenu("Html", "edgedocks.custom.menu");
+  menu.addMenuItem(commandName);
 });
-
-
